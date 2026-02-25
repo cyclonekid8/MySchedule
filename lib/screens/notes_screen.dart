3e1a7d72e14bf1b42fde79173b33ff85516ad71a
cyclonekid8 <muzafarshahmaricab@gmail.com>
2026-02-25 10:49:15 +0800

    Fix notes save button cut off by nav bar

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -43,7 +43,7 @@ class _NotesScreenState extends State<NotesScreen> {
         return Padding(
           padding: EdgeInsets.only(
             left: 20, right: 20, top: 20,
-            bottom: MediaQuery.of(ctx).viewInsets.bottom + 20,
+            bottom: MediaQuery.of(ctx).viewInsets.bottom + MediaQuery.of(ctx).padding.bottom + 20,
           ),
           child: Column(
             mainAxisSize: MainAxisSize.min,
